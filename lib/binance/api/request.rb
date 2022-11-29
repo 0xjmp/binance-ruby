972eeef4d952fd38a03b616d9fb3b1ba8e3d4572
Steven Chang <stevenchang5000@gmail.com>
2022-11-29 16:23:22 +1000

    request: Use ENV variables for quotaguard_user and quotaguard_password

--- lib/binance/api/request.rb
+++ lib/binance/api/request.rb
@@ -2,7 +2,7 @@ module Binance
   module Api
     class Request
       include HTTParty
-      http_proxy 'http://us-east-static-09.quotaguard.com', 9293, Rails.application.credentials.quotaguard.user, Rails.application.credentials.quotaguard.password
+      http_proxy 'http://us-east-static-09.quotaguard.com', 9293, ENV["QUOTAGUARD_USER"], ENV["QUOTAGUARD_PASSWORD"]
 
       class << self
         def send!(api_key_type: :none, headers: {}, method: :get, path: "/", params: {}, security_type: :none, tld: Configuration.tld, api_key: nil, api_secret_key: nil)
