f1b9050186eb87da1c56dbb7697be2bdc67465e3
zign <zignmail@gmail.com>
2023-02-26 08:19:46 +0000

    add margin websocket

--- lib/binance/api/user_data_stream.rb
+++ lib/binance/api/user_data_stream.rb
@@ -15,6 +15,11 @@ module Binance
                         security_type: :user_stream, api_key: api_key, api_secret_key: api_secret_key)[:listenKey]
         end
 
+        def margin_start!(api_key: nil, api_secret_key: nil)
+          Request.send!(api_key_type: :none, method: :post, path: "/sapi/v1/userDataStream",
+                        security_type: :user_stream, api_key: api_key, api_secret_key: api_secret_key)[:listenKey]
+        end
+
         def stop!(listen_key: nil, api_key: nil, api_secret_key: nil)
           raise Error.new(message: "listen_key is required") if listen_key.nil?
           Request.send!(api_key_type: :none, method: :delete, path: "/api/v1/userDataStream",
